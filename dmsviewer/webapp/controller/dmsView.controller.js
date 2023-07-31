26a2d75e7915ec44d9370da3f24bfff4608508f2
Neil Prince <neil.prince@complianterp.com>
2023-08-01 09:48:44 +1000

    controller repository working

--- dmsviewer/webapp/controller/dmsView.controller.js
+++ dmsviewer/webapp/controller/dmsView.controller.js
@@ -9,18 +9,18 @@ sap.ui.define([
 
         return Controller.extend("ns.dmsviewer.controller.dmsView", {
             onInit: function () {
-                // this.getView().byId("sdi-container").setSettings({
-                //     "repositoryId": "77345a30-a136-4518-9f12-3c67da93da47",
-                //     "objectId": "3bbe018000520e5358df9b46",
-                //     "forceLoad": false                    
-                // })
+                this.getView().byId("sdi-container").setSettings({
+                    "repositoryId": "77345a30-a136-4518-9f12-3c67da93da47",
+                    "objectId": "3bbe018000520e5358df9b46",
+                    "forceLoad": false                    
+                })
             },
-            // onComponentCreated: function(oEvent) {
-            //     this._oDocumentTable = oEvent.getParameter("component");
-            //     // OPTIONAL: set a folder as home folder
-            //     this._oDocumentTable.setHomeFolder("77345a30-a136-4518-9f12-3c67da93da47");
-            //     // OPTIONAL: request a navigation to a repository & folder during runtime
-            //     this._oDocumentTable.requestNavigationAndOpen("77345a30-a136-4518-9f12-3c67da93da47", "3bbe018000520e5358df9b46");
-            // }            
+            onComponentCreated: function(oEvent) {
+                this._oDocumentTable = oEvent.getParameter("component");
+                // OPTIONAL: set a folder as home folder
+                this._oDocumentTable.setHomeFolderId("3bbe018000520e5358df9b46");
+                // OPTIONAL: request a navigation to a repository & folder during runtime
+                this._oDocumentTable.requestNavigationAndOpen("77345a30-a136-4518-9f12-3c67da93da47", "3bbe018000520e5358df9b46");
+            }            
         });
     });
